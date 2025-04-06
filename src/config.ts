24aa4d2f6c3371be79e84e4f618d5937b791a5a8
Christopher Kim <1006807+cswkim@users.noreply.github.com>
2025-04-06 16:35:58 -0400

    feat: add media type for setting the accept header and api versioning

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ dotenv.config();
 export const config = {
   discogs: {
     apiUrl: process.env.DISCOGS_API_URL,
+    mediaType: process.env.DISCOGS_MEDIA_TYPE,
     personalAccessToken: process.env.DISCOGS_PERSONAL_ACCESS_TOKEN,
     userAgent:
       process.env.DISCOGS_USER_AGENT_APP_NAME && process.env.DISCOGS_USER_AGENT_URL
@@ -24,16 +25,20 @@ export const config = {
 export function validateConfig(): void {
   const missingVars: string[] = [];
 
+  if (!config.discogs.apiUrl) {
+    missingVars.push('DISCOGS_API_URL');
+  }
+
+  if (!config.discogs.mediaType) {
+    missingVars.push('DISCOGS_MEDIA_TYPE');
+  }
+
   if (!config.discogs.personalAccessToken) {
     missingVars.push('DISCOGS_PERSONAL_ACCESS_TOKEN');
   }
 
   if (!config.discogs.userAgent) {
     missingVars.push('DISCOGS_USER_AGENT_APP_NAME and/or DISCOGS_USER_AGENT_URL');
-  }
-
-  if (!config.discogs.apiUrl) {
-    missingVars.push('DISCOGS_API_URL');
   }
 
   if (config.server.port === undefined) {
