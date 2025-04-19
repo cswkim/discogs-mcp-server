003955fb74d6a6a19247412eee2e94a4ac018359
Christopher Kim <1006807+cswkim@users.noreply.github.com>
2025-04-19 16:04:30 -0400

    fix: update changeset commit script to be a plain .js file

---
+++ scripts/changeset-commit-message.js
@@ -0,0 +1,14 @@
+async function getAddMessage(changeset) {
+  return `docs(changeset): ${changeset.summary}`;
+}
+
+async function getVersionMessage(releasePlan) {
+  return `chore(release): version bump to v${releasePlan.releases[0].newVersion}`;
+}
+
+const defaultCommitFunctions = {
+  getAddMessage,
+  getVersionMessage,
+};
+
+export default defaultCommitFunctions;
